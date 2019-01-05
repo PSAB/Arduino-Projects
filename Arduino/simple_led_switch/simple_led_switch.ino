6aad6d10c3334f198d383e8552909545b22ee468
Pavan S Sabnis <pvnsbns@gmail.com>
2019-01-04 18:20:29 -0800

    Add additional LED in series with initial LED to monitor program status

--- Arduino/simple_led_switch/simple_led_switch.ino
+++ Arduino/simple_led_switch/simple_led_switch.ino
@@ -1,4 +1,5 @@
 const int LED_pin = 8;
+const int LED_pin_2 = 10;
 const int BUTTON_pin = 7;
 
 
@@ -17,23 +18,34 @@ void loop() {
     Serial.print("button is pressed!");
     // Turn on the LED
     digitalWrite(LED_pin, HIGH);
+
+        // Blink the LED so we can pinpoint the issue: 
+
+   digitalWrite(LED_pin_2, HIGH); 
+  //Wait 1 second
+  delay(100);
+  //Turn the LED Off
+  digitalWrite(LED_pin_2, LOW);
+  //Wait 1 second
+  delay(100);
+    
   }
 
 
   // if the Button is not pressed:
   else {
     Serial.print("No Button Pressed!");
-    digitalWrite(LED_pin, LOW);
+    //digitalWrite(LED_pin, LOW);
   
 
     // Blink the LED so we can pinpoint the issue: 
 
    digitalWrite(LED_pin, HIGH); 
   //Wait 1 second
-  delay(500);
+  delay(100);
   //Turn the LED Off
   digitalWrite(LED_pin, LOW);
   //Wait 1 second
-  delay(500);
+  delay(100);
   }
 }
